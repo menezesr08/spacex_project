709d82ab475c5aeaed2d62ae8f171bcf3addc152
Rohan Menezes <menezesr08@gmail.com>
2022-10-14 12:04:16 +0100

    Updated models to include patch in mission

--- lib/models/mission.dart
+++ lib/models/mission.dart
@@ -1,14 +1,19 @@
+import 'package:spacex_project/models/patch.dart';
+
 class Mission {
     Mission({
         required this.name,
          required this.rocket,
+         required this.patch
     });
 
     final String name;
     final String rocket;
+    final Patch patch;
 
     factory Mission.fromJson(Map<String, dynamic> json) => Mission(
         name: json[0]["name"] ?? "",
         rocket: json[0]["rocket"] ?? "",
+        patch: Patch.fromJson(json["links"]["patch"]),
     );
 }
