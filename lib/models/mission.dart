083205d414ff2124e1032f75fd1f3e18bc05ff0f
Rohan Menezes <menezesr08@gmail.com>
2022-10-14 15:44:42 +0100

    updated models

--- lib/models/mission.dart
+++ lib/models/mission.dart
@@ -1,14 +1,16 @@
 class Mission {
-  Mission({
-    required this.name,
-    required this.rocket,
-  });
+  Mission(
+      {required this.name, required this.rocket, required this.flightNumber, required this.launchDate});
 
-  final String name;
-  final String rocket;
+  final String? name;
+  final String? rocket;
+  final int? flightNumber;
+  final String? launchDate;
 
   factory Mission.fromJson(Map<String, dynamic> json) => Mission(
-        name: json["name"] ?? "",
-        rocket: json["rocket"] ?? "",
+        name: json["name"],
+        rocket: json["rocket"],
+        flightNumber: json["flight_number"],
+        launchDate: json["date_utc"]
       );
 }
--- lib/models/patch.dart
+++ lib/models/patch.dart
@@ -1,15 +1,14 @@
 class Patch {
-    Patch({
-        required this.small,
-         required this.large,
-    });
+  Patch({
+    required this.small,
+    required this.large,
+  });
 
-    final String small;
-    final String large;
+  final String? small;
+  final String? large;
 
-    factory Patch.fromJson(Map<String, dynamic> json) => Patch(
-        small: json["small"] ?? "",
-        large: json["large"] ?? "",
-    );
-
+  factory Patch.fromJson(Map<String, dynamic> json) => Patch(
+        small: json["small"],
+        large: json["large"],
+      );
 }
