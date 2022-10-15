fd1d80293f9b8b1265b9027e81da44b0651a5bd3
Rohan Menezes <menezesr08@gmail.com>
2022-10-15 11:45:48 +0100

    moved changenotifierprovider to root levle

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:spacex_project/UI/launches_data.dart';
-import 'package:spacex_project/UI/launches_page.dart';
+import 'package:spacex_project/UI/upcoming_launches_data.dart';
+import 'package:spacex_project/UI/screens/upcoming_launches_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => UpcomingLaunchesData(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const UpcomingLaunchesScreen(),
       ),
-      home: ChangeNotifierProvider(
-          create: (context) => LaunchData(), child: const LaunchesPage()),
     );
   }
 }
