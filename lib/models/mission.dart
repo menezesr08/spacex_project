2e833d8487ef3efb6d4f67846034d399a1326a35
Rohan Menezes <menezesr08@gmail.com>
2022-10-15 11:45:31 +0100

    added properties to mission model

--- lib/models/mission.dart
+++ lib/models/mission.dart
@@ -1,16 +1,25 @@
 class Mission {
-  Mission(
-      {required this.name, required this.rocket, required this.flightNumber, required this.launchDate});
+  Mission({
+    required this.name,
+    required this.rocket,
+    required this.id,
+    required this.launchDate,
+    required this.flightNumber,
+    required this.youtubeId,
+  });
 
+  final String? id;
   final String? name;
   final String? rocket;
-  final int? flightNumber;
   final String? launchDate;
+  final int? flightNumber;
+  final String? youtubeId;
 
   factory Mission.fromJson(Map<String, dynamic> json) => Mission(
-        name: json["name"],
-        rocket: json["rocket"],
-        flightNumber: json["flight_number"],
-        launchDate: json["date_utc"]
-      );
+      name: json["name"],
+      rocket: json["rocket"],
+      id: json["id"],
+      launchDate: json["date_utc"],
+      flightNumber: json["flight_number"],
+      youtubeId: json["links"]["youtube_id"]);
 }
